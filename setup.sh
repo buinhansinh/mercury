56be666d0343007f8ab4a336252762efba3439da
terence <terence@gmail.com>
2013-10-11 13:30:38 +0800

    Make a logs directory that isn't checked in to git. Update distribute and setuptools.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 sudo apt-get install nginx supervisor mysql-server mysql-client mysql-workbench libmysqlclient-dev python-dev build-essentials python-pip python-virtualenv python-zc.buildout
-#easy_install -U distribute setuptools
 virtualenv --no-site-packages /web/mercury
 cd /web/mercury
+mkdir logs
 source bin/activate
+pip install -U distribute setuptools
 python bootstrap.py -d -v 2.1.1
 bin/buildout
 sudo cp supervisor.conf /etc/supervisor/conf.d/mercury.conf
